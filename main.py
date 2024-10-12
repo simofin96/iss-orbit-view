cfd44864665ba2f4b51ca9f9784f70b03adfbb63
simofin96 <65085506+simofin96@users.noreply.github.com>
2024-10-12 16:55:21 +0200

    Added current location of ISS

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 http://open-notify.org/
 """
 import requests
+import datetime
 
 ### People in ISS ###
 response = requests.get("http://api.open-notify.org/astros.json")
@@ -16,3 +17,15 @@ print(f"People currently on the ISS: {len(list_people_on_ISS)}")
 print("\nNames:")
 for astronaut in list_people_on_ISS:
     print(astronaut)
+
+### ISS location ###
+response = requests.get("http://api.open-notify.org/iss-now.json")
+
+timestamp_unix_format = response.json()["timestamp"]
+readable_date = datetime.datetime.fromtimestamp(timestamp_unix_format) 
+current_latitude = response.json()["iss_position"]["latitude"]
+current_longitude = response.json()["iss_position"]["longitude"]
+
+print(f"\nTimestamp: {readable_date}")
+print(f"Current latitude: {current_latitude}")
+print(f"Current longitude: {current_longitude}")
