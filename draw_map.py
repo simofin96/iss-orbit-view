b340cc67a70e391f57045734e514dd18f35760ed
simofin96 <65085506+simofin96@users.noreply.github.com>
2024-12-24 01:56:09 +0100

    Added automatic plot update

--- draw_map.py
+++ draw_map.py
@@ -1,6 +1,14 @@
 from mpl_toolkits.basemap import Basemap
 import numpy as np
 import matplotlib.pyplot as plt
+import ISS_data
+
+def update_ISS_location(fig, m, iss, new_lat, new_lon):
+    # Update ISS location
+    x, y = m(new_lon, new_lat)
+    iss.set_data(x, y)
+    fig.canvas.draw()
+    plt.pause(0.1)
 
 def draw_earth(current_iss_latitude, current_iss_longitude):
     # llcrnrlat,llcrnrlon,urcrnrlat,urcrnrlon
@@ -9,12 +17,12 @@ def draw_earth(current_iss_latitude, current_iss_longitude):
     # lat_ts is the latitude of true scale.
     # resolution = 'c' means use crude resolution coastlines.
 
-    plt.figure(figsize=(12, 8)) 
+    fig = plt.figure(figsize=(12, 8)) 
     m = Basemap(projection='merc',llcrnrlat=-80,urcrnrlat=80,\
                 llcrnrlon=-180,urcrnrlon=180,lat_ts=20,resolution='c')
 
     x, y = m(current_iss_longitude, current_iss_latitude)
-    m.plot(x, y, color="red", marker="o", markersize=10, label='ISS Position')
+    iss, = m.plot(x, y, color="red", marker="o", markersize=10, label='ISS Position')
 
     m.drawcoastlines()
     m.fillcontinents(color='lightgreen',lake_color='aqua')
@@ -26,4 +34,4 @@ def draw_earth(current_iss_latitude, current_iss_longitude):
     m.drawmapboundary(fill_color='aqua')
 
     plt.title("Mercator Projection")
-    plt.show()
+    return fig, m, iss
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 """
 http://open-notify.org/
+https://matplotlib.org/basemap/stable/users/index.html
+https://www.astroviewer.net/iss/en/
 """
+import time
 import requests
 import datetime
 import draw_map
@@ -32,4 +35,18 @@ print(f"Current latitude: {current_iss_latitude}")
 print(f"Current longitude: {current_iss_longitude}")
 
 ### Draw Earth ###
-draw_map.draw_earth(current_iss_latitude, current_iss_longitude)
+fig, m, iss = draw_map.draw_earth(current_iss_latitude, current_iss_longitude)
+
+while(True):
+    response = requests.get("http://api.open-notify.org/iss-now.json")
+    timestamp_unix_format = response.json()["timestamp"]
+    readable_date = datetime.datetime.fromtimestamp(timestamp_unix_format) 
+    current_iss_latitude = float(response.json()["iss_position"]["latitude"])
+    current_iss_longitude = float(response.json()["iss_position"]["longitude"])
+    
+    draw_map.update_ISS_location(fig, m, iss, current_iss_latitude, current_iss_longitude)
+    
+    print(f"\nTimestamp: {readable_date}")
+    print(f"Current latitude: {current_iss_latitude}")
+    print(f"Current longitude: {current_iss_longitude}")
+    time.sleep(5)
