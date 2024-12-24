e9c6522afc7bf75aac74b8228a4b14a5d1985e7e
simofin96 <65085506+simofin96@users.noreply.github.com>
2024-12-24 16:31:45 +0100

    Added information about the ISS on the plot

--- draw_map.py
+++ draw_map.py
@@ -1,14 +1,15 @@
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 
-def update_iss_location(fig, m, iss, new_lat, new_lon):
+def update_iss_location(fig, m, iss, new_timestamp, new_lat, new_lon, iss_location_text):
     """This function updates the position of the ISS on the map."""
     x, y = m(new_lon, new_lat)
     iss.set_data(x, y)
+    iss_location_text.set_text(f"ISS Coordinates\n\nLatitude: {new_lat}°\nLongitude: {new_lon}°\n{new_timestamp}")
     fig.canvas.draw()
     plt.pause(0.1)
 
-def draw_earth(iss_latitude, iss_longitude):
+def draw_earth(timestamp, iss_latitude, iss_longitude, number_people_on_iss, list_people_on_iss):
     """This function draws the Earth and a dot that indicates the current position of the ISS. 
     llcrnrlat, llcrnrlon, urcrnrlat, urcrnrlon are the lat/lon values of the lower left 
     and upper right corners of the map. lat_ts is the latitude of true scale.
@@ -25,6 +26,15 @@ def draw_earth(iss_latitude, iss_longitude):
     m.drawmapboundary(fill_color='aqua')
     # m.drawparallels(np.arange(-90.,91.,30.)) # draw parallels
     # m.drawmeridians(np.arange(-180.,181.,60.)) # draw meridians
-    plt.title("Mercator Projection")
-    
-    return fig, m, iss
+    plt.title("ISS location - Mercator Projection", fontweight="bold")
+    plt.subplots_adjust(bottom=0.2, top=0.95, left=0.05, right=0.8)
+
+    # Crew info in the figure
+    fig.text(0.87, 0.75, f"Number of crew members\n\n{number_people_on_iss}", fontweight="bold", ha="center")
+    string_list_people_on_iss = "\n".join(list_people_on_iss) # a single string is created containing each name, separated by \n.
+    fig.text(0.87, 0.4, f"Crew members\n\n{string_list_people_on_iss}", fontweight="bold", ha="center")
+
+    # ISS location in the figure
+    iss_location_text = fig.text(0.4, 0.08, f"ISS Coordinates\n\nLatitude: {iss_latitude}°\nLongitude: {iss_longitude}°\n{timestamp}", fontweight="bold", ha="center")
+
+    return fig, m, iss, iss_location_text
--- main.py
+++ main.py
@@ -14,9 +14,9 @@ number_people_on_iss, list_people_on_iss = iss_api_calls.get_people_in_iss()
 timestamp, iss_latitude, iss_longitude = iss_api_calls.get_iss_location()
 
 ### Draw Earth and auto-update plot ###
-fig, m, iss = draw_map.draw_earth(iss_latitude, iss_longitude)
+fig, m, iss, iss_location_text = draw_map.draw_earth(timestamp, iss_latitude, iss_longitude, number_people_on_iss, list_people_on_iss)
 
 while(True):
     timestamp, iss_latitude, iss_longitude = iss_api_calls.get_iss_location()
-    draw_map.update_iss_location(fig, m, iss, iss_latitude, iss_longitude)
+    draw_map.update_iss_location(fig, m, iss, timestamp, iss_latitude, iss_longitude, iss_location_text)
     time.sleep(5) # recommended delay between consecutive calls in the documentation.
