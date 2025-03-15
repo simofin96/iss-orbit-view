b86eef43a6febbb571e439f2215f59b17a69d2fa
simofin96 <65085506+simofin96@users.noreply.github.com>
2025-03-15 17:52:18 +0100

    Added error handling

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ https://www.astroviewer.net/iss/en/
 import time
 import draw_map
 import iss_api_calls
+from pydantic import ValidationError
 
-### People in ISS ###
-number_people_on_iss, list_people_on_iss = iss_api_calls.get_people_in_iss()
+try:
+    ### APIs first data extraction ###
+    list_people_on_iss = iss_api_calls.get_people_in_iss() # people aboard the ISS
+    timestamp, iss_latitude, iss_longitude = iss_api_calls.get_iss_location() # ISS location and timestamp
 
-### ISS location ###
-timestamp, iss_latitude, iss_longitude = iss_api_calls.get_iss_location()
+    ### Draw Earth and auto-update plot ###
+    fig, m, iss, iss_location_text = draw_map.draw_earth(timestamp, iss_latitude, iss_longitude, list_people_on_iss)
 
-### Draw Earth and auto-update plot ###
-fig, m, iss, iss_location_text = draw_map.draw_earth(timestamp, iss_latitude, iss_longitude, number_people_on_iss, list_people_on_iss)
+    while(True):
+        timestamp, iss_latitude, iss_longitude = iss_api_calls.get_iss_location()
+        draw_map.update_iss_location(fig, m, iss, timestamp, iss_latitude, iss_longitude, iss_location_text)
+        time.sleep(5) # recommended delay between consecutive calls in the documentation.
 
-while(True):
-    timestamp, iss_latitude, iss_longitude = iss_api_calls.get_iss_location()
-    draw_map.update_iss_location(fig, m, iss, timestamp, iss_latitude, iss_longitude, iss_location_text)
-    time.sleep(5) # recommended delay between consecutive calls in the documentation.
+except ValidationError as e:
+        print("Validation failed:")
+        for error in e.errors():
+            print(f" - Field: {error['loc']}, Error: {error['msg']}")
+            
+except Exception as e:
+    print(f"Unexpected exception: {e}")
