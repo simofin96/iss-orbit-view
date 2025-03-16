a02ebffaeb54bcfad4b5bdbe56591ef9f4921268
simofin96 <65085506+simofin96@users.noreply.github.com>
2025-03-16 16:14:50 +0100

    Enhanced plot aesthetics

--- draw_map.py
+++ draw_map.py
@@ -1,6 +1,6 @@
-from mpl_toolkits.basemap import Basemap
+import numpy as np
 import matplotlib.pyplot as plt
-
+from mpl_toolkits.basemap import Basemap
 
 def update_iss_trajectory(fig, map, trajectory_plot, trajectory_lat, trajectory_lon):
     """This function updates the ISS trajectory on the map."""
@@ -29,25 +29,38 @@ def draw_earth(timestamp, iss_latitude, iss_longitude, people_on_iss_list):
                 llcrnrlon=-180, urcrnrlon=180, lat_ts=20, resolution='c')
 
     iss_x, iss_y = map(iss_longitude, iss_latitude)
-    iss_plot, = map.plot(iss_x, iss_y, color="red", marker="o", markersize=10, label='ISS Position')
+    
+    trajectory_plot, = map.plot([iss_x], [iss_y], linestyle="-", color="darkblue", linewidth=4, label="ISS Trajectory")
+    iss_plot, = map.plot(iss_x, iss_y, color="red", marker="o", markersize=12, label='ISS Position')
+    
+    map.drawcoastlines(linewidth=1, color='gray')
+    map.drawmapboundary(fill_color='lightblue')
+    map.fillcontinents(color='lightgreen', lake_color='lightblue')
+    map.drawparallels(np.arange(-90.,91.,30.), labels=[1,0,0,0], fontsize=10, color='gray')  # draw parallels
+    map.drawmeridians(np.arange(-180.,181.,60.), labels=[0,0,0,1], fontsize=10, color='gray') # draw meridians
+    plt.title("ISS location - Mercator Projection", fontweight="bold", fontsize=16)
 
-    trajectory_plot, = map.plot([iss_x], [iss_y], linestyle="-", color="blue", linewidth=3, label="ISS Trajectory")
-
-    map.drawcoastlines()
-    map.fillcontinents(color='lightgreen', lake_color='aqua')
-    map.drawmapboundary(fill_color='aqua')
-    # m.drawparallels(np.arange(-90.,91.,30.)) # draw parallels
-    # m.drawmeridians(np.arange(-180.,181.,60.)) # draw meridians
-    plt.title("ISS location - Mercator Projection", fontweight="bold")
     plt.subplots_adjust(bottom=0.2, top=0.95, left=0.05, right=0.8)
 
     # Crew info in the figure
-    fig.text(0.87, 0.75, f"Number of crew members\n\n{len(people_on_iss_list)}", fontweight="bold", ha="center")
+    fig.text(0.88, 0.72, f"Number of crew members\n\n{len(people_on_iss_list)}", 
+             fontweight="bold", 
+             fontsize=12,
+             ha="center",
+             bbox=dict(facecolor='beige', alpha=0.5))
     people_on_iss_list_string = "\n".join(people_on_iss_list) # a single string is created containing each name, separated by \n.
-    fig.text(0.87, 0.4, f"Crew members\n\n{people_on_iss_list_string}", fontweight="bold", ha="center")
+    fig.text(0.88, 0.27, f"Crew members\n\n{people_on_iss_list_string}", 
+             fontweight="bold", 
+             fontsize=12,
+             ha="center",
+             bbox=dict(facecolor='beige', alpha=0.5))
 
     # ISS location in the figure
-    iss_location_text = fig.text(0.4, 0.08, f"ISS Coordinates\n\nLatitude: {iss_latitude}°\nLongitude: {iss_longitude}°\n{timestamp}", fontweight="bold", ha="center")
+    iss_location_text = fig.text(0.42, 0.03, f"ISS Coordinates\n\nLatitude: {iss_latitude}°\nLongitude: {iss_longitude}°\n{timestamp}", 
+                                 fontweight="bold",
+                                 fontsize=12, 
+                                 ha="center", 
+                                 bbox=dict(facecolor='beige', alpha=0.5))
 
     # Show the figure
     fig.canvas.draw()
